42700e31060cd120efda405f38cb39d5eba59015
jasmartdev <jasmartdevregistry@gmail.com>
2026-07-31 11:35:18 +0700

    Update health_check_server.py

--- health_check_server.py
+++ health_check_server.py
@@ -4,5 +4,5 @@ def run_health_check_server():
     Back4app uses HTTP, so bind to standard HTTP logic on the requested port
     server_address = ('0.0.0.0', 443)
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
-    # print("Health check server running on port 443...")
+    print("Health check server running on port 443...")
     httpd.serve_forever()
